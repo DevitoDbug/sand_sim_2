454e6e34af85c228946e0936dee7299d7b5f11d5
devitodbug <davidochiengy@gmail.com>
2026-09-04 12:16:13 +0300

    initialize main game file with basic setup and configuration

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use macroquad::prelude::*;
+
+use crate::engine::{
+    game::Game,
+    consts::{BLOCK_SIZE, COLS, ROWS},
+};
+
+mod engine;
+
+#[macroquad::main(config())]
+async fn main() {
+    let mut game = Game::new();
+    game.render().await;
+}
+
+fn config() -> Conf {
+    Conf {
+        window_title: String::from("Sand game"),
+        window_height: BLOCK_SIZE as i32 * ROWS,
+        window_width: BLOCK_SIZE as i32 * COLS,
+        window_resizable: false,
+
+        ..Default::default()
+    }
+}
+
